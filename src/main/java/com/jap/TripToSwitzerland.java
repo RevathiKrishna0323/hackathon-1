2475e9aacf442c713a7f257cc0e784f174f8792d
Revathi G <revathikrishna0323@gmail.com>
2022-08-22 21:13:45 +0530

    Added toUpperCase();

--- src/main/java/com/jap/TripToSwitzerland.java
+++ src/main/java/com/jap/TripToSwitzerland.java
@@ -1,17 +1,24 @@
 package com.jap;
 
-public class TripToSwitzerland{
-    public static void main(String[] args){
+import java.util.Arrays;
+
+public class TripToSwitzerland {
+    public static void main(String[] args) {
         String[] cities = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
         int[] distances = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
         // declare and initialise an onject of the class
         TripToSwitzerland trip = new TripToSwitzerland();
-
-
-        //The Cities are not given in same case, convert all the cities in uppercase.
-        public String[] citiesUpper(cities){
-            return cities.toUpperCase();
-
-        }
+        //method calling
+        String upperCase = trip.convertToUpperCase(Arrays.toString(cities));
+        System.out.println("Converted Upper Case Cities = " + upperCase);
     }
+
+    //The Cities are not given in same case, convert all the cities in uppercase.
+    public String convertToUpperCase(String cities) {
+        return cities.toUpperCase();
+    }
+
+
+    //public String[] toUpperCase(String[] cities){
+    //if(cities.equals(""))
 }
