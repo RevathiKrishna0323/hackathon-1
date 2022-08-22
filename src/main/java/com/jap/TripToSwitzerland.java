c604fc670cf74b5690e65a76a24888957bab3c1b
Revathi G <revathikrishna0323@gmail.com>
2022-08-22 18:49:36 +0530

    declared and initialised String and integer arrays

--- src/main/java/com/jap/TripToSwitzerland.java
+++ src/main/java/com/jap/TripToSwitzerland.java
@@ -2,7 +2,8 @@ package com.jap;
 
 public class TripToSwitzerland{
     public static void main(String[] args){
-
+        String[] cities = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+        int[] distances = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
 
     }
 
