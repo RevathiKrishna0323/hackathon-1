3f7e6b8874dcb6959e384e4e460f0bdbf5d0029b
Revathi G <revathikrishna0323@gmail.com>
2022-08-22 18:41:55 +0530

    Added trip to switzerland class and main method.

---
+++ src/main/java/com/jap/TripToSwitzerland.java
@@ -0,0 +1,10 @@
+package com.jap;
+
+public class TripToSwitzerland{
+    public static void main(String[] args){
+
+
+    }
+
+
+}
--- src/test/java/com/jap/SortingCitiesTest.java
+++ src/test/java/com/jap/SortingCitiesTest.java
@@ -1,19 +1,52 @@
 package com.jap;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class SortingCitiesTest {
+    SortingCities sortingCities;
 
     @Before
     public void setUp()  {
+        sortingCities = new SortingCities();
     }
 
     @After
     public void tearDown()  {
     }
 
-    //write all the test cases here
+    @Test
+    public void stringArray() {
+        //arrange
+        String city = "Bern";
+        //act
+        String expectedResult = "Bern";
+        String actualResult = sortingCities.main(new String[]{"Bern"});
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void intArray() {
+        //arrange
+        int distance= 138;
+        //act
+        int expectedResult = 138;
+        String actualResult = String.valueOf(138);
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void convertToUpper() {
+        //arrange
+        String city = "Bern";
+        //act
+        String expectedResult = "BERN";
+        String actualResult = sortingCities.toUpperCase();
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
