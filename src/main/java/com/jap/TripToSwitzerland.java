af8534d8d7880432fa5b26b8922870808c51cee9
Revathi G <revathikrishna0323@gmail.com>
2022-08-22 22:49:02 +0530

    Added CityGraterThanOrEqual270Km method.

--- src/main/java/com/jap/TripToSwitzerland.java
+++ src/main/java/com/jap/TripToSwitzerland.java
@@ -11,22 +11,35 @@ public class TripToSwitzerland {
         //method calling
         String upperCase = trip.convertToUpperCase(Arrays.toString(cities));
         System.out.println("Converted Upper Case Cities = " + upperCase);
+        // calling cityAt52Km
+        String result = trip.CityAt52Km(cities);
+        System.out.println("City at 52Km = " + result);
+        // calling cityGraterThanOeEqualTo270;
+        String result2 = trip.CityGraterThanOrEqual270Km(distances, cities);
+        System.out.println("City grater than or equal to 270Km = " + result2);
     }
 
     //The Cities are not given in same case, convert all the cities in uppercase.
     public String convertToUpperCase(String cities) {
         return cities.toUpperCase();
     }
+
     //Find the city which is 52 kms from Zurich
-    public String[] CityAt52Km(String[] cities, int[] distances) {
-        String cityAt52Km = " ";
-        for (int index = 0; index < distances.length; index++) {
-            if (index == 52) ;
-            cityAt52Km = cities[index];
+    public String CityAt52Km(int[] distances, String[] cities) {
+        String city52 = " ";
+        for (int i = 0; i < distances.length; i++) {
+            if (distances[i] == 52) ;
+            city52 = cities[i];
         }
-        return cityAt52Km();
+        return city52;
     }
 
-    //public String[] toUpperCase(String[] cities){
-    //if(cities.equals(""))
+    public String CityGraterThanOrEqual270Km(int[] distance, String[] cities) {
+        String cityFartest = " ";
+        for (int i = 0; i < distance.length; i++) {
+            if (distance[i] >= 270) ;
+            cityFartest = cities[i];
+        }
+        return cityFartest;
+    }
 }
