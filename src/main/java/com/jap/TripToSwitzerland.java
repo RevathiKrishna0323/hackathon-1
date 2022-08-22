4d05f723607549718de3d9e6bae4f5a3c6345e0d
Revathi G <revathikrishna0323@gmail.com>
2022-08-22 19:01:43 +0530

    declared and initialised an object of the class

--- src/main/java/com/jap/TripToSwitzerland.java
+++ src/main/java/com/jap/TripToSwitzerland.java
@@ -4,8 +4,14 @@ public class TripToSwitzerland{
     public static void main(String[] args){
         String[] cities = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
         int[] distances = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        // declare and initialise an onject of the class
+        TripToSwitzerland trip = new TripToSwitzerland();
 
+
+        //The Cities are not given in same case, convert all the cities in uppercase.
+        public String[] citiesUpper(cities){
+            return cities.toUpperCase();
+
+        }
     }
-
-
 }
